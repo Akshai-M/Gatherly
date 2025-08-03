dc9cdba7c85311ccc302f46c12b28c7258cf0472
akshai-m <mylapuramakshaikumar@gmail.com>
2025-08-03 12:31:51 +0530

    feat: layout.tsx top-level root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} bg-dark-2 antialiased`}
       >
         {children}
       </body>
